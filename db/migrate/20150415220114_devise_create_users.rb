a948b0586f2c0bcce7af4df38ebbfc25bc69676d
mshamild <mshd91@gmail.com>
2015-04-25 00:56:36 +0300

    carrierwave complete

--- db/migrate/20150415220114_devise_create_users.rb
+++ db/migrate/20150415220114_devise_create_users.rb
@@ -35,8 +35,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
-    add_index :users, :name
-    add_index :users, :surname
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
