76ab08a2d94459c2657cb05ec9371f43da42decc
mshamild <mshd91@gmail.com>
2015-05-08 01:46:17 +0300

    look

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
-    redirect_to article_path(@article)
+    redirect_to articles_url(@article)
 end
  
 private
--- db/migrate/20150415220114_devise_create_users.rb
+++ db/migrate/20150415220114_devise_create_users.rb
@@ -6,7 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :surname,              null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.integer :age
+      t.string :vkpage
+      t.string :mobile
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
